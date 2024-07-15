940cd63b74193de7e03b1aa2b0e68b151d42ba05
Abosede Toriola <aptoriola@unionbankng.com>
2024-07-15 21:22:15 +0100

    sidebar completed

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,16 @@
 import { useState } from "react";
 import { NavBar } from "../../components/Shared/NavBar";
-import { ActiveItem, InactiveItem, SideBar } from "../../components/SideBar/SideBar";
+import {
+  ActiveItem,
+  InactiveItem,
+  SideBar,
+} from "../../components/SideBar/SideBar";
 import { Support } from "../Support/Support";
 import { Account } from "../Account/Account";
 import { Category } from "../Category/Category";
 import { SidebarList } from "../../components/SideBar/SideBar";
+import { Search } from "react-bootstrap-icons";
+
 export const Dashboard = () => {
   const [active, setActive] = useState("Account");
 
@@ -13,10 +19,24 @@ export const Dashboard = () => {
       <div className="bg-slate-50 w-dvw min-h-dvh">
         <NavBar />
         <div className="grid grid-cols-7 gap-8 mx-32 mt-32">
-          <div className="col-span-2">
+          <div className="col-span-2 w-[300px]">
             <>
               <div className="bg-white border border-slate-200 rounded-lg">
-                <ul className="font-semibold text-base">
+                <div className="my-3 text-sm flex items-center justify-center">
+                  <form className="flex w-4/5 h-8 rounded bg-white border border-slate-200 ">
+                    <Search
+                      className="absolute h-4 w-4 flex ml-3 my-2"
+                      color="#D0D5DD"
+                    />
+                    <input
+                      type="search"
+                      name="searchParam"
+                      className="w-full py-1 px-4 text-gray-800 mx-5 focus:outline-none"
+                      placeholder="Search..."
+                    />
+                  </form>
+                </div>
+                <ul className="font-semibold text-base my-2">
                   {SidebarList.map((value, key) => {
                     return (
                       <li
@@ -29,15 +49,17 @@ export const Dashboard = () => {
                         ) : (
                           <>
                             {" "}
-                            <div className="py-2 mx-12 flex hover:bg-slate-100 hover:p-1 hover:pointer-events-auto hover:rounded-md">
-                              <div className="py-1.5 text-gray-600">
+                            <div className="hover:text-blue-900 hover:cursor-auto">
+                            <div className="py-2 px-1 mx-12 flex text-sm">
+                              <div className="py-1 text-gray-600">
                                 {value.icon}
                               </div>
                               <p className="px-2 py-0.5 ">{value.title}</p>{" "}
                             </div>
+                            </div>
+                           
                           </>
                         )}
-                       
                       </li>
                     );
                   })}
@@ -45,7 +67,7 @@ export const Dashboard = () => {
               </div>
             </>
           </div>
-          <div className="col-span-4 mx-2 px-4">
+          <div className="mx-8 px-4 w-[600px]">
             {active === "Account" && <Account />}
             {active === "Category & Pricing" && <Category />}
             {active === "Support" && <Support />}
